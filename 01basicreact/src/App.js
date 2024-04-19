7b6613dc2fb7e7b07ba36d5577ba271258934a4a
Unnati Gavhale <unnati@crepido.com>
2024-04-19 14:15:27 +0200

    code checkin

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Coffee and React | Unnati</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+ 
+
+  return (
+   <h1>Coffee and react with Vite | Unnati</h1>
+  )
+}
+
+export default App
